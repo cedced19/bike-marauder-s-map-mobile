e5ef5d003597abe1335f9e58626b22d1dafd59d3
Cédric JUNG <cedced19@gmail.com>
2017-12-25 12:05:41 +0100

    Use native-base and ask for permissions

--- App.js
+++ App.js
@@ -1,29 +1,96 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { PermissionsAndroid, Alert } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+import { Container, Header, Content, Button, Text, StyleProvider, Body, Title, Form, Input, Item, Label } from 'native-base';
+import getTheme from './native-base-theme/components';
+import material from './native-base-theme/variables/material';
 import SmsListener from 'react-native-android-sms-listener';
+import Contacts from 'react-native-contacts';
 
 export default class App extends React.Component {
+  
   render() {
-    
+    function startListener (params) {
+      PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.READ_SMS, PermissionsAndroid.PERMISSIONS.READ_CONTACTS
+      ]).then(function (data) {
+        if (data['android.permission.READ_SMS'] !== 'granted') {
+          Alert.alert(
+            'Error',
+            'Cannot get permission to read SMS.',
+            [{text: 'OK'}],
+            { cancelable: false }
+          );
+        } else if (data['android.permission.READ_CONTACTS'] !== 'granted') {
+          Alert.alert(
+            'Error',
+            'Cannot get permission to read contacts.',
+            [{text: 'OK'}],
+            { cancelable: false }
+          );
+        } else {
+          Contacts.getAll((err, contacts) => {
+            if(err === 'denied'){
+              Alert.alert(
+                'Error',
+                'An error occurated when reading contacts.',
+                [{text: 'OK'}],
+                { cancelable: false }
+              );
+            } else {
+              Alert.alert(
+                'Info',
+                'The app is now collecting coordinates.',
+                [{text: 'OK'}],
+                { cancelable: false }
+              );
+            }
+          })
+        }
+      }).catch(function () {
+        Alert.alert(
+          'Error',
+          'Cannot get permissions',
+          [{text: 'OK'}],
+          { cancelable: false }
+        );
+      }) 
+      /*
 
-    SmsListener.addListener(message => {
-      console.error(message);
-    });   
+      SmsListener.addListener(message => {
+        console.error(message);
+      }); */
+    }
+
+    
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View> 
+      <StyleProvider style={getTheme(material)}>
+        <Container>
+          <Header>
+            <Body>
+              <Title>Bike marauder's map</Title>
+            </Body>
+          </Header>
+        
+          <Content contentContainerStyle ={{paddingHorizontal: 10 }}>
+            <Text>
+              Please set a server adress to send coordinates.
+            </Text>
+            <Form>
+              <Item floatingLabel>
+                <Label>Server address</Label>
+                <Input />
+              </Item>
+              <Button style={{marginTop: 10 }} onPress={startListener}>
+                <Text>Start collecting coordinates</Text>
+              </Button>
+            </Form>
+           
+             
+            
+          </Content>
+        </Container>
+      </StyleProvider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
